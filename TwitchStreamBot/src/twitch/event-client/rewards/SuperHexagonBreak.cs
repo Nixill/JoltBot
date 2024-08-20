db52d9da084974a60cda3f09974d3d368630b231
Steven <github@steven-h.net>
2024-08-20 02:29:52 -0400

    More

--- TwitchStreamBot/src/twitch/event-client/rewards/SuperHexagonBreak.cs
+++ TwitchStreamBot/src/twitch/event-client/rewards/SuperHexagonBreak.cs
@@ -2,9 +2,8 @@ using TwitchLib.Api.Helix.Models.Channels.GetChannelInformation;
 
 namespace Nixill.Streaming.JoltBot.Twitch.Events.Rewards;
 
-[RewardContainer]
 [CommandContainer]
-public static class SuperHexagonContainer
+public static class SuperHexagonController
 {
   internal static HashSet<string> PlayedRounds = [];
   internal static HashSet<string> WonRounds = [];
@@ -17,43 +16,49 @@ public static class SuperHexagonContainer
       + "flashing lights that may be problematic for some viewers. ⚠️");
     await ctx.MessageAsync("It usually lasts about 3 minutes. Nix's PB "
       + "is just under 5½ minutes.");
-  }
 
+    CurrentRound = level;
+  }
+}
+
+[RewardContainer]
+public static class SuperHexagonRewards
+{
   [ChannelPointsReward("SuperHexagon")]
   [DisableTemporarily]
   [SuperHexagon("hexagon")]
   public static async Task SuperHexagonBreakHexagon(RewardContext ctx)
-    => await SuperHexagonBreak(ctx, "hexagon");
+    => await SuperHexagonController.SuperHexagonBreak(ctx, "hexagon");
 
   [ChannelPointsReward("SuperHexagoner")]
   [DisableTemporarily]
   [SuperHexagon("hexagoner", "hexagon")]
   public static async Task SuperHexagonBreakHexagoner(RewardContext ctx)
-    => await SuperHexagonBreak(ctx, "hexagoner");
+    => await SuperHexagonController.SuperHexagonBreak(ctx, "hexagoner");
 
   [ChannelPointsReward("SuperHexagonest")]
   [DisableTemporarily]
   [SuperHexagon("hexagonest", "hexagoner")]
   public static async Task SuperHexagonBreakHexagonest(RewardContext ctx)
-    => await SuperHexagonBreak(ctx, "hexagonest");
+    => await SuperHexagonController.SuperHexagonBreak(ctx, "hexagonest");
 
   [ChannelPointsReward("SuperHexagonHyper")]
   [DisableTemporarily]
   [SuperHexagon("hyper-hexagon", "hexagon")]
   public static async Task SuperHexagonBreakHyperHexagon(RewardContext ctx)
-    => await SuperHexagonBreak(ctx, "hyper-hexagon");
+    => await SuperHexagonController.SuperHexagonBreak(ctx, "hyper-hexagon");
 
   [ChannelPointsReward("SuperHexagonerHyper")]
   [DisableTemporarily]
   [SuperHexagon("hyper-hexagoner", "hexagoner", "hyper-hexagon")]
   public static async Task SuperHexagonBreakHyperHexagoner(RewardContext ctx)
-    => await SuperHexagonBreak(ctx, "hyper-hexagoner");
+    => await SuperHexagonController.SuperHexagonBreak(ctx, "hyper-hexagoner");
 
   [ChannelPointsReward("SuperHexagonestHyper")]
   [DisableTemporarily]
   [SuperHexagon("hyper-hexagonest", "hexagonest", "hyper-hexagoner")]
   public static async Task SuperHexagonBreakHyperHexagonest(RewardContext ctx)
-    => await SuperHexagonBreak(ctx, "hyper-hexagonest");
+    => await SuperHexagonController.SuperHexagonBreak(ctx, "hyper-hexagonest");
 }
 
 public class SuperHexagonAttribute(string lvl, params string[] prereqs) : LimitAttribute
@@ -64,8 +69,8 @@ public class SuperHexagonAttribute(string lvl, params string[] prereqs) : LimitA
   protected override Task<bool> ConditionCheck(BaseContext ctx, ChannelInformation info)
   {
     if (info.Tags.Any(t => t.Equals("speedrun", StringComparison.InvariantCultureIgnoreCase))) return Task.FromResult(false);
-    if (Prerequisites.Any(p => !SuperHexagonContainer.PlayedRounds.Contains(p))) return Task.FromResult(false);
-    if (SuperHexagonContainer.PlayedRounds.Contains(Level)) return Task.FromResult(false);
+    if (Prerequisites.Any(p => !SuperHexagonController.PlayedRounds.Contains(p))) return Task.FromResult(false);
+    if (SuperHexagonController.PlayedRounds.Contains(Level)) return Task.FromResult(false);
 
     return Task.FromResult(true);
   }
